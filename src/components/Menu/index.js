399df97354cb597d67112e807129b72c2b282b9c
Sol Xavier <solange-xs@hotmail.com>
2020-07-29 20:19:54 -0300

    Setup Inicial para o FrontEndFlix

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/logo.png';
 import './Menu.css';
 import Button from '../Button';
@@ -6,10 +7,10 @@ import Button from '../Button';
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
-                <img className="Logo" src={Logo}  alt="SolFlix Logo"/>
-            </a>
-            <Button as="a" className="ButtonLink" href="/">
+            <Link to="/">
+                <img className="Logo" src={Logo}  alt="FrontEndFlix Logo"/>
+            </Link>
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Adicionar Vídeo
             </Button>
         </nav>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/home';
+import CadastroVideo from './pages/cadastro/video';
+import CadastroCategoria from './pages/cadastro/categoria';
+
+const Pagina404 = () => (<div>Página não encontrada!</div>);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact></Route>
+      <Route path="/cadastro/video" component={CadastroVideo} exact></Route>
+      <Route path="/cadastro/categoria" component={CadastroCategoria} exact></Route>
+      <Route component={Pagina404}></Route>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
